74e96fb7dfaa637c26d52df11ca48da7f887ec92
mkebbachi-sudo <mkebbachi@gmail.com>
2025-12-30 01:22:45 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Watches from "./pages/Watches";
+import WatchDetail from "./pages/WatchDetail";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Order from "./pages/Order";
+import OrderConfirmation from "./pages/OrderConfirmation";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/watches" element={<Watches />} />
+          <Route path="/watch/:id" element={<WatchDetail />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/order/:id" element={<Order />} />
+          <Route path="/order-confirmation" element={<OrderConfirmation />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </HashRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
