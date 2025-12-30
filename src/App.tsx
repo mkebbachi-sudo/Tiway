572eaa2cc23230ecb133303b3d4d1a7c64f685c0
mkebbachi-sudo <mkebbachi@gmail.com>
2025-12-30 01:48:54 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/index";
+import Index from "./pages/Index";
 import Watches from "./pages/Watches";
 import WatchDetail from "./pages/WatchDetail";
 import About from "./pages/About";
